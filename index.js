20178e800843c3fd1e1ed6a74c55d75be46e0d32
DenysPyshniuk <denys.pyshniuk@gmail.com>
2020-12-04 04:26:10 +0000

    Implemented fully working command-line application that help you to find out when International space station going to fly by inthe sky where you are

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
+// const {fetchMyIP, fetchCoordsByIP} = require("./iss");
+// const { fetchCoordsByIP } = require("./iss");
+// const { fetchISSFlyOverTimes } = require('./iss');
+const {nextISSTimesForMyLocation} = require("./iss");
 
+
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// The code below is temporary and can be commented out.
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log("It worked! Returned IP:", ip);
+// });
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// The code below is temporary and can be commented out.
+
+// fetchCoordsByIP("75.157.45.128", (error, coords) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log("It worked! Returned Coords:", coords);
+// });
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// The code below is temporary and can be commented out.
+
+// fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned flyover times:' , passTimes);
+// });
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  printPassTimes(passTimes);
+});
